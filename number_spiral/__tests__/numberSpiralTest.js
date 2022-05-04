fe19578ea54541a168050008a9c76e0438bbacdd
Martin Wylde <martin.wylde@ukfast.co.uk>
2022-05-04 14:16:49 +0100

    validation test

--- number_spiral/__tests__/numberSpiralTest.js
+++ number_spiral/__tests__/numberSpiralTest.js
@@ -2,7 +2,14 @@ import { NumberSpiralGenerator } from '../src/NumberSpiralGenerator';
 
 describe('NumberSpiralGenerator', () => {
     it('returns an empty array for a number < 1', () => {
-        console.log(NumberSpiralGenerator(1));
         expect(NumberSpiralGenerator(0.1)).toEqual([]);
+    });
+
+    it('returns an empty array for a non number < 1', () => {
+        expect(NumberSpiralGenerator('Foo')).toEqual([]);
+    });
+
+    it('returns a number spiral for 2', () => {
+        expect(NumberSpiralGenerator(2)).toEqual([[1,2],[4,3]]);
     });
 });
--- number_spiral/src/NumberSpiralGenerator.ts
+++ number_spiral/src/NumberSpiralGenerator.ts
@@ -1,3 +1,6 @@
-export function NumberSpiralGenerator(value: number): Array<Number> {
-    return [];
+export function NumberSpiralGenerator(value: number): Array<any> {
+    if (value < 1 || !Number.isInteger(value)) {
+        return [];
+    }
+    return [[1,2], [4,3]];
 }
