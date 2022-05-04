2fcc6103a769374e46e6909ee04f92e319a2ef8a
Martin Wylde <martin.wylde@ukfast.co.uk>
2022-05-04 15:47:33 +0100

    solution

--- number_spiral/__tests__/numberSpiralTest.js
+++ number_spiral/__tests__/numberSpiralTest.js
@@ -12,4 +12,8 @@ describe('NumberSpiralGenerator', () => {
     it('returns a number spiral for 2', () => {
         expect(NumberSpiralGenerator(2)).toEqual([[1,2],[4,3]]);
     });
+
+    it('returns a number spiral for 3', () => {
+        expect(NumberSpiralGenerator(3)).toEqual([[1,2,3],[8,9,4],[7,6,5]]);
+    });
 });
--- number_spiral/src/NumberSpiralGenerator.ts
+++ number_spiral/src/NumberSpiralGenerator.ts
@@ -1,6 +1,47 @@
-export function NumberSpiralGenerator(value: number): Array<any> {
+interface pos { x: number; y: number};
+
+export function NumberSpiralGenerator(value: any): Array<any> {
     if (value < 1 || !Number.isInteger(value)) {
         return [];
     }
-    return [[1,2], [4,3]];
+    const spiral = Array(value).fill(null).map(() => Array(value));
+    let total = 1;
+    let top = 0;
+    let right = value -1;
+    let bottom = value -1;
+    let left = 0;
+
+    let direction = 'right';
+
+    while(top <= bottom && left <= right && total <= 1000) {
+        if (direction === 'right') {
+            for(let col = left; col <= right; col++) {
+                spiral[top][col] = total++;
+            }
+            ++top;
+            direction = 'down';
+        } else if (direction === 'down') {
+            for(let row = top; row <= bottom; row++) {
+                spiral[row][right] = total++;
+            }
+            --right;
+            direction = 'left';
+        } else if (direction === 'left') {
+            for(let col = right; col >= left; col--) {
+                spiral[bottom][col] = total++;
+            }
+            --bottom;
+            direction = 'up';
+        } else if (direction === 'up') {
+            for(let row = bottom; row >= top; row--) {
+                spiral[row][left] = total++;
+            }
+            ++left;
+            direction = 'right';
+        }
+    }
+
+    console.log(spiral);
+    return spiral;
 }
+
